887e04c0185e546f3b0bc03e30f56b8e68953301
PahanSanajana <pahansanjana6226@gmail.com>
2025-04-16 11:54:05 +0530

    Add Home component with introduction text and avatar image

--- portfolio_react/src/App.jsx
+++ portfolio_react/src/App.jsx
@@ -1,12 +1,12 @@
-import { Navbar } from "./components/Navbar/Navbar"
+import { Home } from "./components/Home/Home";
+import { Navbar } from "./components/Navbar/Navbar";
 function App() {
-
-
   return (
-    <div className="bg-[#171d32] h-auto w-full overflow-hidden"> 
+    <div className="bg-[#171d32] h-auto w-full overflow-hidden">
       <Navbar />
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ portfolio_react/src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import avatarImg from "../../assets/Me-working-on-a-laptop.png";
+
+export const Home = () => {
+  return (
+    <div className="text-white flex w-full justify-between items-start p-10 md:p-20">
+      <div className="md:w-2/4 md:pt-10">
+        <h1 className="text-3xl md:text-6xl font-bold flex leading-normal tracking-tighter">
+          Hello' I am Sanjana
+        </h1>
+        <p className="text-sm md:text-2xl tracking-tight">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Exercitationem veniam quis dolorum quisquam
+        </p>
+        <button className="mt-5 md:md-10 text-white py02 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 
+        duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
+          Contact Me
+        </button>
+      </div>
+      <div><img className="w-2/5" src={avatarImg} alt="" /></div>
+    </div>
+  );
+};
